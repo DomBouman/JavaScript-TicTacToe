221aef73371638b7c1f5df83e8d0b1169a15a391
Anthony McMellen <anthony@BHB-CSET-18-13.local>
2018-11-12 15:52:48 -0500

    clean slate for tonight

---
+++ function.js
@@ -0,0 +1,21 @@
+//one winner
+let player1 = prompt("what's ya name?");
+let player2 = prompt("what's ya name?");
+let symbol1 = "X";
+let symbol2 = "O";
+let turn = 0;
+let anchor = document.getElementsByTagName("a")
+//randomly selects a player to go first
+let picker = Math.floor(Math.random() * 100);
+//players take turns
+if (turn % 2 === 0) {
+  //replace the contents of a node with the current players name.
+};
+//one symbol per turn
+
+//three in a row wins
+
+//if full, no winner
+
+//cant click in a space taken
+
